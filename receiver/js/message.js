b6d748b50ecae4ee189afd47d38e10d76d347e0d
Joseph Turian <turian@gmail.com>
2014-05-18 02:37:52 -0700

    Small fix

--- receiver/js/message.js
+++ receiver/js/message.js
@@ -6,7 +6,7 @@ dataRef.on('child_added', function(snapshot) {
     if (!snapshot.val().type || snapshot.val().type != "ping") {
         // Send a roundtrip ping, to get an upper bound on the time to send to firebase and receive from firebase.
         // (In fact, we measure *TWO* sends and one receive.)
-        dataRef.push({type: "ping", createdAt: Firebase.ServerValue.TIMESTAMP, receivedAt: int(snapshot.val().createdAt)});
+        dataRef.push({type: "ping", createdAt: Firebase.ServerValue.TIMESTAMP, receivedAt: snapshot.val().createdAt});
     } else {
         console.log("Ping time in ms:" + (snapshot.val().createdAt- snapshot.val().receivedAt));
     }
