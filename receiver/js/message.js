7273d818ecfe3841bd3fc058aac5f3e920691ac1
Joseph Turian <turian@gmail.com>
2014-05-18 02:37:17 -0700

    Added ping time computation

--- receiver/js/message.js
+++ receiver/js/message.js
@@ -3,6 +3,13 @@ dataRef.remove(); // Clear the existing firebase events
 
 dataRef.on('child_added', function(snapshot) {
     //console.log(snapshot.val());
+    if (!snapshot.val().type || snapshot.val().type != "ping") {
+        // Send a roundtrip ping, to get an upper bound on the time to send to firebase and receive from firebase.
+        // (In fact, we measure *TWO* sends and one receive.)
+        dataRef.push({type: "ping", createdAt: Firebase.ServerValue.TIMESTAMP, receivedAt: int(snapshot.val().createdAt)});
+    } else {
+        console.log("Ping time in ms:" + (snapshot.val().createdAt- snapshot.val().receivedAt));
+    }
     if (playing) {
         //console.log("playing");
         senderEvent(snapshot.val());
