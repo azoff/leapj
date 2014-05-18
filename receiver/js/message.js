5d9396296e84faa70e23132772eb00741e7a38db
Windsor Schmidt <windsor.schmidt@gmail.com>
2014-05-17 21:52:38 -0700

    hopefully done with message.js for a while

--- receiver/js/message.js
+++ receiver/js/message.js
@@ -1,36 +1,34 @@
-require(["app"],function(app){
-    var dataRef = new Firebase(config.firebase_room_uri);
-    dataRef.on('child_added', function(snapshot) {
-        //parseMsg(snapshot.val())
-    });
+var dataRef = new Firebase(config.firebase_room_uri);
+dataRef.on('child_added', function(snapshot) {
+    //parseMsg(snapshot.val())
+});
 
-    // send incoming messages to webaudio control handlers
-    function parseMsg(o) {
-        switch(o.type) {
-        case "on":
-            app.enable(o)
-            break;
-        case "off":
-            app.disable(o)
-            break;
-        case "start":
-            app.start(o)
-            break;
-        case "filter":
-            app.filter(o)
-            break;
-        case "volume":
-            app.volume(o)
-            break;
-        case "eq":
-            app.eq(o)
-            break;
-        case "pan":
-            app.pan(o)
-            break;
-        case "reverb":
-            app.reverb(o)
-            break;
-        }
+// send incoming messages to webaudio control handlers
+function parseMsg(o) {
+    switch(o.type) {
+    case "on":
+        app.enable(o)
+        break;
+    case "off":
+        app.disable(o)
+        break;
+    case "start":
+        app.start(o)
+        break;
+    case "filter":
+        app.filter(o)
+        break;
+    case "volume":
+        app.volume(o)
+        break;
+    case "eq":
+        app.eq(o)
+        break;
+    case "pan":
+        app.pan(o)
+        break;
+    case "reverb":
+        app.reverb(o)
+        break;
     }
-})
+}
