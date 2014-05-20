3e1d9ead69b3aef9efca1566b718150f86e85dc7
Nathan Leiby <nathanleiby@gmail.com>
2014-05-19 22:14:31 -0700

    gestures: add a test environment for gestures

---
+++ js/gestures-test-app.js
@@ -0,0 +1,59 @@
+(function(){
+
+	"use strict";
+
+	requirejs.config({
+		paths: {
+			jquery: 'jquery-2.1.1/jquery.min',
+			angular: 'angular-1.2.15/angular',
+			firebase: 'firebase-1.0.15/firebase',
+			detectors: 'core/detectors',
+			gestures: 'core/gestures',
+			pubsub: 'core/pubsub',
+			visuals: 'core/visuals',
+			audio: 'core/audio',
+			user: 'core/user',
+			fingerprint: 'fingerprint/fingerprint',
+			three: 'leap/three',
+			leap: 'leap/leap-0.6.0',
+			leapPlugins: 'leap/leap-plugins-0.1.6',
+			leapHand: 'leap/leap.rigged-hand-0.1.4.min'
+		},
+		shim: {
+			angular: {
+				deps: ['jquery'],
+				exports: 'angular'
+			},
+			three: {
+				exports: 'THREE'
+			},
+			leap: {
+				exports: 'Leap'
+			},
+			leapPlugins: {
+				exports: 'Leap'
+			},
+			leapHand: {
+				deps: ['leap', 'leapPlugins', 'three'],
+				exports: 'Leap'
+			},
+			firebase: {
+				exports: 'Firebase'
+			}
+		}
+	});
+
+	require(['jquery', 'leap', 'detectors'], function($, Leap, detectors) {
+		function publishMotionEvent(event) {
+			console.log("event");
+		}
+
+		function detectMotions(frame) {
+			detectors.space(frame, publishMotionEvent);
+			detectors.pinch(frame, publishMotionEvent);
+		}
+
+		Leap.loop({ enableGestures: true, background: true }, detectMotions);
+	});
+
+})();
